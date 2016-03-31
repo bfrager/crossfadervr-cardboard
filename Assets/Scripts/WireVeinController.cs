dc043e8b34ab6e8898a5acbbf92391c3fa0e562b
Sam <Sameats3d@gmail.com>
2016-03-31 14:08:26 -0700

    trying to get VRDevice to work

---
+++ Assets/Scripts/WireVeinController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WireVeinController : MonoBehaviour {
+
+	public GameObject[] nodes;
+	public List<Transform> cachedNodes;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void GatherAvaliableNodes()
+	{
+		nodes = GameObject.FindGameObjectsWithTag("djNodes");
+
+		Debug.Log(nodes);
+	}
+
+	void CacheNodes(GameObject[] goList)
+	{
+
+	}
+}
