7a162412d3b7ee036e189536182c107a27856bb3
bfrager <btfrager@gmail.com>
2016-04-08 13:40:36 -0700

    added JSON data import apicall.js, printing to console

--- Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
+++ Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
@@ -1,37 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using SimpleJSON;
 
+// based on https://github.com/andyburke/UnityHTTP
+
 public class ApiCall : MonoBehaviour {
     
     private string cfDataParsed;
-    private List<Performance> performances = new List<Performance>();
+    // private List<Performance> performances = new List<Performance>();
 
-	// Use this for initialization
 	void Start () {
 	    HTTP.Request crossFaderData = new HTTP.Request( "get", "http://api.crossfader.fm/v3/performances" );
         crossFaderData.Send( ( request ) => 
         {
-            // parse some JSON, for example:
             JSONObject cfData = new JSONObject( request.response.Text );
             Debug.Log(cfData);
+                        
+        // TODO: PARSE JSON DATA (SIMPLEJSON FUNCTION NOT WORKING)
             // performances = JsonConvert.DeserializeObject<List<Performance>>(cfData);
-            cfDataParsed = JSON.Parse(cfData);
-            foreach(JSONClass performance in cfDataParsed["performances"].AsArray) 
-            {  
-                int id = performance.AsObject["id"].Value;
-                int duration_seconds = performance.AsObject["duration_seconds"].Value;
-                int user_id = performance.AsObject["user_id"].Value;
-                performances.Add(new Performance(id, duration, user_id));
-            }
-            Debug.Log(performances.Count);
-            Debug.Log(performances);
+            // cfDataParsed = JSON.Parse(cfData);
+            // foreach(JSONClass performance in cfDataParsed["performances"].AsArray) 
+            // {  
+            //     int id = performance.AsObject["id"].Value;
+            //     int duration_seconds = performance.AsObject["duration_seconds"].Value;
+            //     int user_id = performance.AsObject["user_id"].Value;
+            //     performances.Add(new Performance(id, duration, user_id));
+            // }
+            // Debug.Log(performances.Count);
+            // Debug.Log(performances);
         });
-        
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
