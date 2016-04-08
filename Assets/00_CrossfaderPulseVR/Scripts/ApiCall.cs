d12f57a498297582e030e54204225f62a74d8616
bfrager <btfrager@gmail.com>
2016-04-08 00:10:35 -0700

    added json object import from crossfader endpoint

---
+++ Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+
+public class ApiCall : MonoBehaviour {
+    
+    private string cfDataParsed;
+    private List<Performance> performances = new List<Performance>();
+
+	// Use this for initialization
+	void Start () {
+	    HTTP.Request crossFaderData = new HTTP.Request( "get", "http://api.crossfader.fm/v3/performances" );
+        crossFaderData.Send( ( request ) => 
+        {
+            // parse some JSON, for example:
+            JSONObject cfData = new JSONObject( request.response.Text );
+            Debug.Log(cfData);
+            // performances = JsonConvert.DeserializeObject<List<Performance>>(cfData);
+            cfDataParsed = JSON.Parse(cfData);
+            foreach(JSONClass performance in cfDataParsed["performances"].AsArray) 
+            {  
+                int id = performance.AsObject["id"].Value;
+                int duration_seconds = performance.AsObject["duration_seconds"].Value;
+                int user_id = performance.AsObject["user_id"].Value;
+                performances.Add(new Performance(id, duration, user_id));
+            }
+            Debug.Log(performances.Count);
+            Debug.Log(performances);
+        });
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/00_CrossfaderPulseVR/Scripts/Performance.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System; //This allows the IComparable Interface
+
+//This is the class you will be storing
+//in the different collections. In order to use
+//a collection's Sort() method, this class needs to
+//implement the IComparable interface.
+// public class Performance : IComparable<Performance>
+// {
+//     public int id;
+//     public int duration_seconds;
+//     public int user_id;
+    
+//     public Performance(int newId, int newDuration_seconds, int newUser_id)
+//     {
+//         id = newId;
+//         duration_seconds = newDuration_seconds;
+//         user_id = newUser_id;
+//     }
+// }
+
+public class Performance
+{
+    public int id { get; set; }
+    public string created_at { get; set; }
+    public string updated_at { get; set; }
+    public int segment_count { get; set; }
+    public List<string> segment_loops { get; set; }
+    public int segment_loop_count { get; set; }
+    public int duration { get; set; }
+    public string title { get; set; }
+    public string slug { get; set; }
+    public string completed_at { get; set; }
+    public int favorites_count { get; set; }
+    public object favorited_at { get; set; }
+    public int listens_count { get; set; }
+    public List<string> tags { get; set; }
+    public int duration_seconds { get; set; }
+    public int user_id { get; set; }
+    public string audio_url { get; set; }
+    public int version { get; set; }
+    public object location { get; set; }
+    public string background_image { get; set; }
+    public int reports_count { get; set; }
+    public int listeners_count { get; set; }
+    public int live_listeners_count { get; set; }
+    public string published_at { get; set; }
+    public bool is_on_main_stage { get; set; }
+}
+
+public class RootObject
+{
+    public List<Performance> performances { get; set; }
+}
