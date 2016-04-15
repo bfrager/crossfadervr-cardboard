600e7dc99f896157e9bcb50a785a82f9caed6ac3
bfrager <btfrager@gmail.com>
2016-04-14 23:39:52 -0700

    added call to parse incoming JSON data into game objects

--- Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
+++ Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using SimpleJSON;
+using LitJson;
+
 
 // based on https://github.com/andyburke/UnityHTTP
 
 public class ApiCall : MonoBehaviour {
     
     private string cfDataParsed;
+    private JsonData itemData;
     // private List<Performance> performances = new List<Performance>();
 
 	void Start () {
@@ -16,7 +19,16 @@ public class ApiCall : MonoBehaviour {
         {
             JSONObject cfData = new JSONObject( request.response.Text );
             Debug.Log(cfData);
-                        
+            accessData(cfData);
+            
+            
+           // LitJson
+            // itemData = JsonMapper.ToObject(cfData);
+            // Debug.Log(itemData["performances"][1]["title"]);
+            // GetItem(783984, "performances");
+            
+            
+           //simpleJSON             
         // TODO: PARSE JSON DATA (SIMPLEJSON FUNCTION NOT WORKING)
             // performances = JsonConvert.DeserializeObject<List<Performance>>(cfData);
             // cfDataParsed = JSON.Parse(cfData);
@@ -31,7 +43,51 @@ public class ApiCall : MonoBehaviour {
             // Debug.Log(performances);
         });
 	}
-	
+
+//access data (and print it)
+void accessData(JSONObject obj)
+            {
+                switch(obj.type)
+                {
+                    case JSONObject.Type.OBJECT:
+                        for(int i = 0; i < obj.list.Count; i++){
+                            string key = (string)obj.keys[i];
+                            JSONObject j = (JSONObject)obj.list[i];
+                            Debug.Log(key);
+                            accessData(j);
+                        }
+                        break;
+                    case JSONObject.Type.ARRAY:
+                        foreach(JSONObject j in obj.list){
+                            accessData(j);
+                        }
+                        break;
+                    case JSONObject.Type.STRING:
+                        Debug.Log(obj.str);
+                        break;
+                    case JSONObject.Type.NUMBER:
+                        Debug.Log(obj.n);
+                        break;
+                    case JSONObject.Type.BOOL:
+                        Debug.Log(obj.b);
+                        break;
+                    case JSONObject.Type.NULL:
+                        Debug.Log("NULL");
+                        break;
+            
+                }
+            }
+
+//LitJson	
+    // JsonData GetItem(int id, string type)
+    // {
+    //     for (int i = 0; i < itemData[type].Count; i++)
+    //     {
+    //         if (itemData[type][i]["id"] == id)
+    //             return itemData[type][i];
+    //     }
+    // }
+    
 	void Update () {
 	
 	}
