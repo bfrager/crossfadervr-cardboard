13ac0ea3442aa085315e37d39582dd496a5dc7ab
bfrager <btfrager@gmail.com>
2016-04-16 21:06:43 -0700

    separated apicall endpoints to individual performance ids

--- Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
+++ Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
@@ -8,32 +8,51 @@ using LitJson;
 
 public class ApiCall : MonoBehaviour {
     
-    private string cfDataParsed;
-    private JsonData itemData;
-    private List<Performance> performances = new List<Performance>();
+    private string url;
+    private string apiEndpoint;
+    // private JsonData itemData;
+    public List<int> performanceIds = new List<int>();
+    private List<JSONObject> performances = new List<JSONObject>();
 
 	void Start () {
-	    HTTP.Request crossFaderData = new HTTP.Request( "get", "http://api.crossfader.fm/v3/performances" );
-        crossFaderData.Send( ( request ) => 
+        //add performance IDs of DJ Nodes to grab
+        performanceIds.AddRange (new int[]{734638, 722131, 734615, 734598, 550352, 709722, 741630, 681878, 734698, 734668, 734734, 722237, 695891, 669405, 695743});
+                
+        // Send Request to Crossfader Server
+        apiEndpoint = "http://api.crossfader.fm/v3/performances/";
+
+	    foreach(int id in performanceIds)
         {
-            JSONObject cfData = new JSONObject( request.response.Text );
-
-        Debug.Log(cfData);
-        accessData(cfData);
-        Debug.Log(cfData["performances"][1]["title"]);
-
-        // GetItem(cfData, 783984, "performances");
+            string idString = id.ToString();
+            url = apiEndpoint + idString;
+            
+            HTTP.Request crossFaderData = new HTTP.Request( "get", url );
+            
+            crossFaderData.Send( ( request ) => 
+            {
+                // Convert response string to JSON object
+                JSONObject cfData = new JSONObject( request.response.Text );
+                
+                performances.Add(cfData);
+                Debug.Log(performances);
+                
+                // access data in response object
+                accessData(cfData["performances"][1]["title"]);
+                
+                // alt syntax to access data
+                JSONObject perf = cfData["performances"][1];
+                Debug.Log(perf["title"]); 
+            });
+        }
         
-        JSONObject perf = cfData["performances"][1];
-        Debug.Log(perf["title"]);
         
-        });
         
         
         // LitJson
         // itemData = JsonMapper.ToObject(cfData);
         // Debug.Log(itemData["performances"][1]["title"]);
         // GetItem(783984, "performances");
+        // GetItem(cfData, 783984, "performances");
         
         
         //simpleJSON             
@@ -53,59 +72,68 @@ public class ApiCall : MonoBehaviour {
         
 	}
 
-//access data (and print it)
-void accessData(JSONObject obj)
-{
-    switch(obj.type)
-    {
-        case JSONObject.Type.OBJECT:
-            for(int i = 0; i < obj.list.Count; i++){
-                string key = (string)obj.keys[i];
-                JSONObject j = (JSONObject)obj.list[i];
-                Debug.Log(key);
-                accessData(j);
-            }
-            break;
-        case JSONObject.Type.ARRAY:
-            foreach(JSONObject j in obj.list){
-                accessData(j);
-            }
-            break;
-        case JSONObject.Type.STRING:
-            Debug.Log(obj.str);
-            break;
-        case JSONObject.Type.NUMBER:
-            Debug.Log(obj.n);
-            break;
-        case JSONObject.Type.BOOL:
-            Debug.Log(obj.b);
-            break;
-        case JSONObject.Type.NULL:
-            Debug.Log("NULL");
-            break;
-    }
-}
-
-// JSONObject GetItem(JSONObject obj, int id, string type)
-// {
-//     for (int i = 0; i < obj[type].Count; i++)
-//     {
-//         if (int.Parse(obj[type][i]["id"]) == id)
-//             return obj[type][i];
-//     }
-// }
-
-//LitJson	
-    // JsonData GetItem(int id, string type)
-    // {
-    //     for (int i = 0; i < itemData[type].Count; i++)
-    //     {
-    //         if (itemData[type][i]["id"] == id)
-    //             return itemData[type][i];
-    //     }
-    // }
-    
-	void Update () {
+    void Update () {
 	
 	}
+    
+    //access data (and print it)
+    void accessData(JSONObject obj)
+    {
+        switch(obj.type)
+        {
+            case JSONObject.Type.OBJECT:
+                for(int i = 0; i < obj.list.Count; i++){
+                    string key = (string)obj.keys[i];
+                    JSONObject j = (JSONObject)obj.list[i];
+                    Debug.Log(key);
+                    accessData(j);
+                }
+                break;
+            case JSONObject.Type.ARRAY:
+                foreach(JSONObject j in obj.list){
+                    accessData(j);
+                }
+                break;
+            case JSONObject.Type.STRING:
+                Debug.Log(obj.str);
+                break;
+            case JSONObject.Type.NUMBER:
+                Debug.Log(obj.n);
+                break;
+            case JSONObject.Type.BOOL:
+                Debug.Log(obj.b);
+                break;
+            case JSONObject.Type.NULL:
+                Debug.Log("NULL");
+                break;
+        }
+    }
+    
+    void AddToList(params int[] list) {
+          for ( int i = 0 ; i < list.Length ; i++ ) {
+             performanceIds.Add(list[i]);
+          }
+     }
+
+//UNUSED CODE (DLL LIBRARY AS ALTERNATE JSON DECODER)
+    // LitJson	
+    //     JSONObject GetItem(JSONObject obj, int id, string type)
+    //     {
+    //         for (int i = 0; i < obj[type].Count; i++)
+    //         {
+    //             if (int.Parse(obj[type][i]["id"]) == id)
+    //                 return obj[type][i];
+    //         }
+    //     }
+
+    //     JsonData GetItem(int id, string type)
+    //     {
+    //         for (int i = 0; i < itemData[type].Count; i++)
+    //         {
+    //             if (itemData[type][i]["id"] == id)
+    //                 return itemData[type][i];
+    //         }
+    //     }
+    
+	
 }
