87537ee37167973656c9be3caa597d805050acde
Gavin Smiley <gavin.race16@gmail.com>
2016-04-16 17:05:37 -0700

    Adjusted CameraFacingBilboard to have the DJ info panels face in. Edited scene so that the UI works properly.

--- Assets/CameraFacingBillboard.cs
+++ Assets/CameraFacingBillboard.cs
@@ -7,12 +7,23 @@ public class CameraFacingBillboard : MonoBehaviour
 
 	void Start()
 	{
-		
+		StartCoroutine(InitialRotation());
 	}
 
 	void Update()
 	{
-		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
-		                 m_Camera.transform.rotation * Vector3.up);
+		/*transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+		                 m_Camera.transform.rotation * Vector3.up);*/
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.position - m_Camera.transform.position),0.2f);
+	}
+
+
+	IEnumerator InitialRotation()
+	{
+		//initial delay
+		yield return new WaitForSeconds(0.5f);
+		transform.rotation = Quaternion.LookRotation(transform.position - m_Camera.transform.position);
+		gameObject.SetActive(false);
 	}
 }
