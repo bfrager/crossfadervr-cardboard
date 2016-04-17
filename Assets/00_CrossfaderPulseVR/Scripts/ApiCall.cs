adf722c2b02d955e88eb5a93cee09332f4860134
bfrager <btfrager@gmail.com>
2016-04-16 19:13:50 -0700

    converting JSON to gameObjects

--- Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
+++ Assets/00_CrossfaderPulseVR/Scripts/ApiCall.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using SimpleJSON;
 using LitJson;
 
 
@@ -11,72 +10,90 @@ public class ApiCall : MonoBehaviour {
     
     private string cfDataParsed;
     private JsonData itemData;
-    // private List<Performance> performances = new List<Performance>();
+    private List<Performance> performances = new List<Performance>();
 
 	void Start () {
 	    HTTP.Request crossFaderData = new HTTP.Request( "get", "http://api.crossfader.fm/v3/performances" );
         crossFaderData.Send( ( request ) => 
         {
             JSONObject cfData = new JSONObject( request.response.Text );
-            Debug.Log(cfData);
-            accessData(cfData);
-            
-            
-           // LitJson
-            // itemData = JsonMapper.ToObject(cfData);
-            // Debug.Log(itemData["performances"][1]["title"]);
-            // GetItem(783984, "performances");
-            
-            
-           //simpleJSON             
-        // TODO: PARSE JSON DATA (SIMPLEJSON FUNCTION NOT WORKING)
-            // performances = JsonConvert.DeserializeObject<List<Performance>>(cfData);
-            // cfDataParsed = JSON.Parse(cfData);
-            // foreach(JSONClass performance in cfDataParsed["performances"].AsArray) 
-            // {  
-            //     int id = performance.AsObject["id"].Value;
-            //     int duration_seconds = performance.AsObject["duration_seconds"].Value;
-            //     int user_id = performance.AsObject["user_id"].Value;
-            //     performances.Add(new Performance(id, duration, user_id));
-            // }
-            // Debug.Log(performances.Count);
-            // Debug.Log(performances);
+
+        Debug.Log(cfData);
+        accessData(cfData);
+        Debug.Log(cfData["performances"][1]["title"]);
+
+        // GetItem(cfData, 783984, "performances");
+        
+        JSONObject perf = cfData["performances"][1];
+        Debug.Log(perf["title"]);
+        
         });
+        
+        
+        // LitJson
+        // itemData = JsonMapper.ToObject(cfData);
+        // Debug.Log(itemData["performances"][1]["title"]);
+        // GetItem(783984, "performances");
+        
+        
+        //simpleJSON             
+        // TODO: PARSE JSON DATA (SIMPLEJSON FUNCTION NOT WORKING)
+        //     performances = JsonConvert.DeserializeObject<List<Performance>>(cfData);
+        //     cfDataParsed = JSON.Parse(cfData);
+            
+        //     foreach(JSONClass performance in cfData["performances"]) 
+        //     {  
+        //         int id = performance.AsObject["id"].Value;
+        //         int duration_seconds = performance.AsObject["duration_seconds"].Value;
+        //         int user_id = performance.AsObject["user_id"].Value;
+        //         performances.Add(new Performance(id, duration, user_id));
+        //     }
+        //     Debug.Log(performances.Count);
+        //     Debug.Log(performances);
+        
 	}
 
 //access data (and print it)
 void accessData(JSONObject obj)
-            {
-                switch(obj.type)
-                {
-                    case JSONObject.Type.OBJECT:
-                        for(int i = 0; i < obj.list.Count; i++){
-                            string key = (string)obj.keys[i];
-                            JSONObject j = (JSONObject)obj.list[i];
-                            Debug.Log(key);
-                            accessData(j);
-                        }
-                        break;
-                    case JSONObject.Type.ARRAY:
-                        foreach(JSONObject j in obj.list){
-                            accessData(j);
-                        }
-                        break;
-                    case JSONObject.Type.STRING:
-                        Debug.Log(obj.str);
-                        break;
-                    case JSONObject.Type.NUMBER:
-                        Debug.Log(obj.n);
-                        break;
-                    case JSONObject.Type.BOOL:
-                        Debug.Log(obj.b);
-                        break;
-                    case JSONObject.Type.NULL:
-                        Debug.Log("NULL");
-                        break;
-            
-                }
+{
+    switch(obj.type)
+    {
+        case JSONObject.Type.OBJECT:
+            for(int i = 0; i < obj.list.Count; i++){
+                string key = (string)obj.keys[i];
+                JSONObject j = (JSONObject)obj.list[i];
+                Debug.Log(key);
+                accessData(j);
             }
+            break;
+        case JSONObject.Type.ARRAY:
+            foreach(JSONObject j in obj.list){
+                accessData(j);
+            }
+            break;
+        case JSONObject.Type.STRING:
+            Debug.Log(obj.str);
+            break;
+        case JSONObject.Type.NUMBER:
+            Debug.Log(obj.n);
+            break;
+        case JSONObject.Type.BOOL:
+            Debug.Log(obj.b);
+            break;
+        case JSONObject.Type.NULL:
+            Debug.Log("NULL");
+            break;
+    }
+}
+
+// JSONObject GetItem(JSONObject obj, int id, string type)
+// {
+//     for (int i = 0; i < obj[type].Count; i++)
+//     {
+//         if (int.Parse(obj[type][i]["id"]) == id)
+//             return obj[type][i];
+//     }
+// }
 
 //LitJson	
     // JsonData GetItem(int id, string type)
---
+++ Assets/00_CrossfaderPulseVR/Scripts/ApiCallJS.js
@@ -0,0 +1,37 @@
+﻿// #pragma strict
+
+// import SimpleJSON;
+
+// private var jsonData;  
+// private var employees = [];
+// public var targetScript = gameObject.GetComponent(ApiCall);
+
+// function Start () {
+    
+//     var jsonData = targetScript.cfData;
+    
+//     Debug.Log(jsonData);
+    
+//     // foreach(JSONClass employee in jsonData["employees"].AsArray) {  
+//     //     string name = employee.AsObject["name"].Value;
+//     //     string age = employee.AsObject["age"].Value;
+//     //     string salary = employee.AsObject["salary"].Value;
+//     //     employees.Add(new Employee(name, age, salary));
+//     // }
+    
+//     // for(var performance in jsonData["performances"].AsArray) 
+//     // {  
+//     //     int id = performance.AsObject["id"].Value;
+//     //     int duration_seconds = performance.AsObject["duration_seconds"].Value;
+//     //     int user_id = performance.AsObject["user_id"].Value;
+//     //     performances.Add(new Performance(id, duration, user_id));
+//     // }
+//     // Debug.Log(performances.Count);
+//     // Debug.Log(performances);
+
+    
+// }
+
+// function Update () {
+
+// }
